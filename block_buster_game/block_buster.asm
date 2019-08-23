b716846886cdc203a71f7b5259399412ba6814e1
lcbcfoo <lcbc.lucascastro@gmail.com>
2019-08-23 16:43:49 -0300

    Added some utilities functions. Module, is_negative and invert

--- block_buster_game/block_buster.asm
+++ block_buster_game/block_buster.asm
@@ -6,7 +6,7 @@ PRG_COUNT = 1 ;1 = 16KB, 2 = 32KB
 MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
 
 ; bar horizontal size
-bar_size = 20
+BAR_SIZE = 20
 
 ;   Refer to this for the following constants
 ;
@@ -18,10 +18,43 @@ bar_size = 20
 ;===================================================  end of screen (0)
 
 ; bar surface level (where the ball is supposed to hit)
-bar_surface = 20
+BAR_SURFACE = 20
 ; Lava y level. If the ball goes under this point -> game over
-lava_surface = 10
+LAVA_SURFACE = 11
 
+LEFT_LIMIT = 10
+RIGHT_LIMIT = 246
+UP_LIMIT = 180
+
+; Default values
+BALL_X = 10
+BALL_Y = 10
+BALL_VX = 1
+BALL_VY = 1
+
+BAR_X = 100
+MOVE_BAR_SELECT = 0
+
+SCORE = 0
+RECORD = 0
+LIVES = 3
+
+HITS_LAVA = 0
+HITS_BAR = 0
+
+BRICKS_HEIGHT = 10
+BRICKS_WIDTH = 30
+
+FIRST_BRICK_Y = 120
+
+BRICKS0 = 10
+BRICKS1 = 11
+BRICKS2 = 12
+BRICKS3 = 13
+BRICKS4 = 14
+BRICKS5 = 15
+BRICKS6 = 16
+BRICKS7 = 17
 
 ;----------------------------------------------------------------
 ; variables
@@ -35,38 +68,32 @@ lava_surface = 10
    ;MyVariable1 .dsb 3
 
     ; bar X position
-    bar_position_x .dsb 100
+    bar_x .dsb 1
 
     ; 0 -> don't move
     ; 1 -> move left
     ; 2 -> move right
-    move_bar_select .dsb 0
+    move_bar_select .dsb 1
 
-    ball_position_x .dsb 100
-    ball_position_y .dsb 50
-    ball_speed_x .dsb 3
-    ball_speed_y .dsb 3
+    ball_x .dsb 1
+    ball_y .dsb 1
+    ball_vx .dsb 1
+    ball_vy .dsb 1
 
-    score .dsb 0
-    lives .dsb 3
+    score .dsb 1
+    record .dsb 1
+    lives .dsb 1
 
     ; Flag used by check_hits_lava. If set to one -> ball hits lava
-    hits_lava .dsb 0
+    hits_lava .dsb 1
 
     ; Flag used by check_hits_bar. If set to one -> ball hits bar
-    hits_bar .dsb 0
+    hits_bar .dsb 1
 
     ; Each bit corresponds to one brick. If the bit is set the corresponding
     ; brick is in the screen
     ; least significant bit corresponds to brick 0
-    bricks0 .dsb 255
-    bricks1 .dsb 255
-    bricks2 .dsb 255
-    bricks3 .dsb 255
-    bricks4 .dsb 255
-    bricks5 .dsb 255
-    bricks6 .dsb 255
-    bricks7 .dsb 255
+    bricks .dsb 8
     
 
    .ende
@@ -94,19 +121,73 @@ lava_surface = 10
 ; program bank(s)
 ;----------------------------------------------------------------
 
-   .base $10000-(PRG_COUNT*$4000)
+   .org $C000
 
 Reset:
 
-   ;NOTE: initialization code goes here
-   JMP check_hits_lava
+    ; Initialize variables with default value
+    LDA     #BALL_X
+    STA     ball_x     
+    LDA     #BALL_Y
+    STA     ball_y
+    LDA     #BALL_VX
+    STA     ball_vx
+    LDA     #BALL_VY
+    STA     ball_vy
+
+    LDA     #BAR_X
+    STA     bar_x
+    LDA     #MOVE_BAR_SELECT
+    STA     move_bar_select
+
+    LDA     #SCORE
+    STA     score
+    LDA     #RECORD
+    STA     record
+    LDA     #LIVES
+    STA     lives
+
+    LDA     #HITS_LAVA
+    STA     hits_lava
+    LDA     #HITS_BAR
+    STA     hits_bar
+
+    LDX     #1
+    LDA     #BRICKS0
+    STA     bricks,X
+    INX
+    LDA     #BRICKS1
+    STA     bricks,X
+    INX
+    LDA     #BRICKS2
+    STA     bricks,X
+    INX
+    LDA     #BRICKS3
+    STA     bricks,X
+    INX
+    LDA     #BRICKS4
+    STA     bricks,X
+    INX
+    LDA     #BRICKS5
+    STA     bricks,X
+    INX
+    LDA     #BRICKS6
+    STA     bricks,X
+    INX
+    LDA     #BRICKS7
+    STA     bricks,X
+    
+    
+    JMP check_hits_lava
 
 NMI:
 
-   ;NOTE: NMI code goes here
+    ;NOTE: NMI code goes here
+    
+    JMP NMI
 
 IRQ:
-
+    JMP Reset
    ;NOTE: IRQ code goes here
 
 
@@ -114,52 +195,113 @@ IRQ:
 ; -----------------------------------------------------------------------------
 ; begin foooooo
 
+
+
+; Utilities
+
+; Register Y -> 1 if value in A register is negative, else to Y -> 0
+is_negative:
+	CMP #$7F
+	BPL IS_NEGATIVE_NEG_LABEL
+	LDY #0
+	RTS
+IS_NEGATIVE_NEG_LABEL:
+	LDY #1
+	RTS
+; end is_negative
+
+
+; Invert value in A register. Equivalent to A = -A
+invert:
+	EOR #$FF
+	CLC
+	ADC #1
+	RTS
+;end invert
+
+
+; Set A to the module of the value in A. A = |A|
+module:
+	PHA
+	JSR is_negative
+	BNE MODULE_NEG_LABEL
+	PLA
+	RTS
+MODULE_NEG_LABEL:
+	PLA
+	JSR invert
+	RTS
+;end module
+
+
+
 ; Change location of the ball based on horizontal and vertical speed.
 move_ball:
+    CLC                             ; clean carry
+    LDA     ball_x                  ; load ball x into A
+    ADC     ball_vx                 ; sum it with ball_vx
+    STA     ball_x                  ; save ball_x to variable
+    CLC
+    LDA     ball_y                  ; load ball y into A
+    ADC     ball_vy                 ; sum it with ball_vy
+    STA     ball_y                  ; save ball_y to variable
+    RTS
+; end mode_ball
+
 
 ; Change position of the bar depending on user input.
 move_bar:
 
+; end move_bar
+
+
+reflect_y:
+    LDA     ball_vy                 ; load ball vy into A
+    SEC     
+    SBC     ball_vy                 ; subtract it 2 times, the result will be
+    SEC
+    SBC     ball_vy                 ; -ball_vy
+    RTS
+
 
 ; Checks if the ball hits the bar.
-check_hits_bar:
-    LDA     ball_position_y         ; load ball y into A
-    SBC     bar_surface             ; subtract bar surface level
+check_hits_something:
+    LDA     ball_y                  ; load ball y into A
+    CMP     #BAR_SURFACE            ; compare with bar surface level
     BMI     hits_bar_label1         ; if result < 0 (is at same level or under
                                     ; bar_surface) goto hits_bar_label1
-    LDA     #$0
-    STA     hits_bar                ; else sets hits_bar to 0 and return
-    RTS                             
+    CMP     #UP_LIMIT               ; compare with up limit
+    BPL     
+    RTS                         
+
 
 hits_bar_label1:
     JSR     check_hits_lava         ; check if ball hit lava
-    LDA     hits_lava               ; load hits_lava into A
-    CMP     #$1                     ;
-    BNE     check_bar_horizontally  ; if hits_lava != 1 need to test ball X
-    JMP     game_over               ; else -> game over
 
-check_bar_horizontally:
     ; TODO
+
+;end check_hits_bar
 
 
 ; Checks if the ball hits one of the bricks.
 check_hits_brick:
 
+;end check_hits_brick
+
 
 ; Checks if the ball hits the lava.
 check_hits_lava:
-    LDA     ball_position_y         ; load ball y into A
-    SBC     lava_surface           ; subtract with lava level
+    LDA     ball_y                  ; load ball y into A
+    CMP     #LAVA_SURFACE           ; compare with lava level
     BMI     hits_lava_check_label   ; if result is < 0 goto check_label
-    RTS                             ; else just return (is_dead is 0, alive)
+    RTS                             ; else just return)
 hits_lava_check_label:
-    LDA     #$1                     ; set is_dead to 1 (dead)
-    STA     hits_lava
-    RTS                             ; return
+    JMP     game_over
 
 
 ; Checks if the call hits the walls.
 check_hits_walls:
+    LDA     ball_y                  
 
 ; end foooooo
 ;-----------------------------------------------------------------------------
