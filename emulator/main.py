b8eccdbfa0caa533e502672b91e4ade0f40ad34d
Ignácio Espinoso Ribeiro <ignacioespinoso@gmail.com>
2019-09-03 13:28:20 -0300

    Splits hex string every 2 bytes

--- emulator/main.py
+++ emulator/main.py
@@ -2,7 +2,6 @@ import sys
 import argparse
 import os
 import subprocess
-import binascii
 from cpu import *
 
 def main():
@@ -10,7 +9,6 @@ def main():
 
     argParser.add_argument('input_file_path', help='The path to ROM file that the emulator will run.')
     argParser.add_argument('-g', '--debug', help='Run the file on debug mode (prints instructions).')
-    # argParser.add_argument('-d', '--display', help='Print the output file on stdout', action='store_true')
 
     args = argParser.parse_args()
     input_file = args.input_file_path
@@ -24,15 +22,13 @@ def main():
     input_file = input_file.replace(".asm", ".bin")
     print(input_file)
     with open(input_file, 'rb') as i:
-        data = i.read()
-        hexa = binascii.hexlify(data)
+        # Gets data as hex string
+        data = i.read().hex()
+        # Splits every 2 bytes
+        hex_data = [data[i:i+2] for i in range(0, len(data), 2)]
     
     # Emulate binary
-    cpu(hexa)
-
-    # Show on stdout if requested to
-    # if args.display:
-    #     print(code)
+    cpu(hex_data)
 
 if __name__ == '__main__':
     main()
