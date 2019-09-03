52dc48f25b18004c25269f29c0002a81e8fae9ed
Ignácio Espinoso Ribeiro <ignacioespinoso@gmail.com>
2019-09-03 15:09:44 -0300

    Sets default output path

--- emulator/README.md
+++ emulator/README.md
@@ -1,7 +1,13 @@
 # How to run
 
-Run the following command on the terminal:
+For basic usage, run the following command on the terminal:
 
 ```console
 python3 main.py [REPOSITORY_PATH]/speedpong_3000_game/speedpong_3000.asm
+```
+
+For help and other options, run:
+
+```console
+python3 main.py -h
 ```
--- emulator/cpu/cpu.py
+++ emulator/cpu/cpu.py
@@ -1,4 +1,13 @@
 import sys
 
-def cpu(lines):
-    print(lines)
+def cpu(lines, output_file):
+    # Writes txt file if output_file is set
+    if output_file is not None:
+        print("Writing file:", output_file)
+        with open(output_file, 'w') as f:
+            for element in lines:
+                f.write("%s\n" % element)
+    else:
+        print("Printing lines on terminal:")
+        for element in lines:
+            print(element)
