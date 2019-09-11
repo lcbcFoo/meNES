4ebe4e10db8d0738767cf3891b3b3a7a526034fd
Ignácio Espinoso Ribeiro <ignacioespinoso@gmail.com>
2019-09-11 15:26:28 -0300

    adds absolute functions for each instruction

--- emulator/cpu/modules/absolute.py
+++ emulator/cpu/modules/absolute.py
@@ -2,28 +2,72 @@ from collections import namedtuple
 
 #Se pc + 1 for maior ou igual ao tamanho da pagina, passamos
 class Absolute():
-    def __init__(self):
-        AbsoluteADC = namedtuple('6D')
-        AbsoluteAND = namedtuple('2D')
-        AbsoluteASL = namedtuple('0E')
-        AbsoluteBIT = namedtuple('2C')
-        AbsoluteCMP = namedtuple('CD')
-        AbsoluteCPX = namedtuple('EC')
-        AbsoluteCPY = namedtuple('CC')
-        AbsoluteDEC = namedtuple('CE')
-        AbsoluteEOR = namedtuple('4D')
-        AbsoluteINC = namedtuple('EE')
-        AbsoluteJMP = namedtuple('4C')
-        AbsoluteJSR = namedtuple('20')
-        AbsoluteLDA = namedtuple('AD')
-        AbsoluteLDX = namedtuple('AE')
-        AbsoluteLDY = namedtuple('AC')
-        AbsoluteLSR = namedtuple('4E')
-        AbsoluteORA = namedtuple('0D')
-        AbsoluteROL = namedtuple('2E')
-        AbsoluteROR = namedtuple('6E')
-        AbsoluteSBC = namedtuple('ED')
-        AbsoluteSTA = namedtuple('8D')
-        AbsoluteSTX = namedtuple('8E')
-        AbsoluteSTY = namedtuple('8C')
-}
+
+    def absolute_adc(self, oper):
+        pass
+
+    def absolute_and(self, oper):
+        pass
+
+    def absolute_asl(self, oper):
+        pass
+
+    def absolute_bit(self, oper):
+        pass
+
+    def absolute_cmp(self, oper):
+        pass
+
+    def absolute_cpx(self, oper):
+        pass
+
+    def absolute_cpy(self, oper):
+        pass
+
+    def absolute_dec(self, oper):
+        pass
+
+    def absolute_eor(self, oper):
+        pass
+
+    def absolute_inc(self, oper):
+        pass
+
+    def absolute_jmp(self, oper):
+        pass
+
+    def absolute_jsr(self, oper):
+        pass
+
+    def absolute_lda(self, oper):
+        pass
+
+    def absolute_ldx(self, oper):
+        pass
+
+    def absolute_ldy(self, oper):
+        pass
+
+    def absolute_lsr(self, oper):
+        pass
+
+    def absolute_ora(self, oper):
+        pass
+
+    def absolute_rol(self, oper):
+        pass
+
+    def absolute_ror(self, oper):
+        pass
+
+    def absolute_sbc(self, oper):
+        pass
+
+    def absolute_sta(self, oper):
+        pass
+
+    def absolute_stx(self, oper):
+        pass
+
+    def absolute_sty(self, oper):
+        pass
