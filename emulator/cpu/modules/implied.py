0a37005d3b9ee21f81fe4f597d16fdad93736abe
Daniel Helú <danielhelu@gmail.com>
2019-09-18 14:38:19 -0300

    Add implied class with functions prototypes

---
+++ emulator/cpu/modules/implied.py
@@ -0,0 +1,54 @@
+class Implied(self):
+    def imp_brk(self, oper):
+        pass
+    def imp_nop(self, oper):
+        pass
+    def imp_rti(self, oper):
+        pass
+    def imp_rts(self, oper):
+        pass
+
+    def imp_clc(self, oper):
+        pass
+    def imp_sec(self, oper):
+        pass
+    def imp_cli(self, oper):
+        pass
+    def imp_sei(self, oper):
+        pass
+    def imp_clv(self, oper):
+        pass
+    def imp_cld(self, oper):
+        pass
+    def imp_sed(self, oper):
+        pass
+
+    def imp_txs(self, oper):
+        pass
+    def imp_tsx(self, oper):
+        pass
+    def imp_pha(self, oper):
+        pass
+    def imp_pla(self, oper):
+        pass
+    def imp_php(self, oper):
+        pass
+    def imp_plp(self, oper):
+        pass
+
+    def imp_tax(self, oper):
+        pass
+    def imp_txa(self, oper):
+        pass
+    def imp_dex(self, oper):
+        pass
+    def imp_inx(self, oper):
+        pass
+    def imp_tay(self, oper):
+        pass
+    def imp_tya(self, oper):
+        pass
+    def imp_dey(self, oper):
+        pass
+    def imp_iny(self, oper):
+        pass
