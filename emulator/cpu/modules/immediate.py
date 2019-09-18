0b01926bb1560034f27c53dd58179daa181709ec
Daniel Helú <danielhelu@gmail.com>
2019-09-18 16:09:48 -0300

    Add all immediate class functions

--- emulator/cpu/modules/immediate.py
+++ emulator/cpu/modules/immediate.py
@@ -1,23 +1,107 @@
+from flag_handler import *
+
 class Immediate(self):
-    def imd_adc(self, oper):
-        pass
-    def imd_and(self, oper):
-        pass
-    def imd_cmp(self, oper):
-        pass
-    def imd_cpx(self, oper):
-        pass
+    def __init__(self, cpu, mem, decoder):
+        self.cpu = cpu
+        self.mem = mem
+        self.decoder = decoder
+        self.fh = FlagHandler(cpu)
+
+    def imd_adc(self):
+        reg_a = self.cpu.a
+        immediate = self.decoder.immediate
+        carry = self.cpu.c
+        result = reg_a + immediate + carry
+        result_8b = self.fh.getActualNum(result)
+        self.cpu.a = result_8b
+        self.fh.SetCarry(result)
+        self.fh.SetOverflow(reg_a, immediate, result_8b)
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
+    def imd_and(self):
+        reg_a = self.cpu.a
+        immediate = self.decoder.immediate
+        result = reg_a & immediate
+        result_8b = self.fh.getActualNum(result)
+        self.cpu.a = result_8b
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
+    def imd_cmp(self):
+        reg_a = self.cpu.a
+        immediate = self.decoder.immediate
+        result = reg_a - immediate
+        result_8b = self.fh.getActualNum(result)
+        self.fh.SetCarry(result)
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
+    def imd_cpx(self):
+        reg_x = self.cpu.x
+        immediate = self.decoder.immediate
+        result = reg_x - immediate
+        result_8b = self.fh.getActualNum(result)
+        self.fh.SetCarry(result)
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
     def imd_cpy(self, oper):
-        pass
+        reg_y = self.cpu.y
+        immediate = self.decoder.immediate
+        result = reg_y - immediate
+        result_8b = self.fh.getActualNum(result)
+        self.fh.SetCarry(result)
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
     def imd_eor(self, oper):
-        pass
+        reg_a = self.cpu.a
+        immediate = self.decoder.immediate
+        result = reg_a ^ immediate
+        result_8b = self.fh.getActualNum(result)
+        self.cpu.a = result_8b
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
     def imd_lda(self, oper):
-        pass
+        immediate = self.decoder.immediate
+        result_8b = self.fh.getActualNum(immediate)
+        self.cpu.a = result_8b
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
     def imd_ldx(self, oper):
-        pass
+        immediate = self.decoder.immediate
+        result_8b = self.fh.getActualNum(immediate)
+        self.cpu.x = result_8b
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
     def imd_ldy(self, oper):
-        pass
+        immediate = self.decoder.immediate
+        result_8b = self.fh.getActualNum(immediate)
+        self.cpu.y = result_8b
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
     def imd_ora(self, oper):
-        pass
+        reg_a = self.cpu.a
+        immediate = self.decoder.immediate
+        result = reg_a | immediate
+        result_8b = self.fh.getActualNum(result)
+        self.cpu.a = result_8b
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
+
     def imd_sbc(self, oper):
-        pass
+        reg_a = self.cpu.a
+        immediate = self.decoder.immediate
+        carry = self.cpu.c
+        result = reg_a - immediate - carry
+        result_8b = self.fh.getActualNum(result)
+        self.cpu.a = result_8b
+        self.fh.SetCarry(result)
+        self.fh.SetOverflow(reg_a, immediate, result_8b)
+        self.fh.SetNegative(result_8b)
+        self.fh.SetZero(result_8b)
