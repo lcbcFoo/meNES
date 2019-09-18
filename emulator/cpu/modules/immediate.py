9cad09d1c0a72847e6bddaa724687a23393ec2b2
Daniel Helú <danielhelu@gmail.com>
2019-09-18 14:37:53 -0300

    Add immediate class with functions prototypes

---
+++ emulator/cpu/modules/immediate.py
@@ -0,0 +1,23 @@
+class Immediate(self):
+    def imd_adc(self, oper):
+        pass
+    def imd_and(self, oper):
+        pass
+    def imd_cmp(self, oper):
+        pass
+    def imd_cpx(self, oper):
+        pass
+    def imd_cpy(self, oper):
+        pass
+    def imd_eor(self, oper):
+        pass
+    def imd_lda(self, oper):
+        pass
+    def imd_ldx(self, oper):
+        pass
+    def imd_ldy(self, oper):
+        pass
+    def imd_ora(self, oper):
+        pass
+    def imd_sbc(self, oper):
+        pass
