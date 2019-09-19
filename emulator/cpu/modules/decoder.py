98a6af60e03adad2c3f4f5639d3a203e4b58fe76
Laura <politi.laura.o@gmail.com>
2019-09-19 18:03:06 -0300

    tiny fix

--- emulator/cpu/modules/decoder.py
+++ emulator/cpu/modules/decoder.py
@@ -25,7 +25,7 @@ class Decoder():
         # Absolute ############################################################
         # "full_addr" is the address obtained by concatenating "high"|"low" to
         # get the complete 16-bit address.
-        full_addr = (high * 2**8) + low
+        full_addr = (high << 8) + low
 
         # absolute: ADC oper -- "content" has the value inside the full address.
         self.content = self.mem_bus.read(full_addr)
@@ -45,21 +45,21 @@ class Decoder():
         # indirect: JMP (oper)
         # -- "pointer_addr" is the address where the program will jump to.
         point_low, point_high = self.mem_bus.read(full_addr, 2)
-        self.pointer_addr = (point_high * 2**8) + point_low
+        self.pointer_addr = (point_high << 8) + point_low
 
         # Indexed indirect
         # (indirect,X): ADC (oper,X)
         # -- "pointer_content_x" is the value used by the instruction.
         ind_addr_x = low + self.cpu.x
         point_low_x, point_high_x = self.mem_bus.read(ind_addr_x, 2)
-        pointer_addr_x = (point_high_x * 2**8) + point_low_x
+        pointer_addr_x = (point_high_x << 8) + point_low_x
         self.pointer_content_x = self.mem_bus.read(pointer_addr_x)
 
         # Indirect indexed
         # (indirect),Y  ADC (oper),Y
         # -- "pointer_content_y" is the value used by the instruction.
         point_low_y, point_high_y = self.mem_bus.read(low, 2)
-        pointer_addr_y = (point_high_y * 2**8) + point_low_y + self.cpu.y
+        pointer_addr_y = (point_high_y << 8) + point_low_y + self.cpu.y
         self.pointer_content_y = self.mem_bus.read(pointer_addr_y)
 
         # #####################################################################
