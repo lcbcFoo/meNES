f00f0c4fe6cf80ccad913ac834a376d9905a96f7
Laura <politi.laura.o@gmail.com>
2019-09-26 20:48:44 -0300

    Fix page crossing on decoder

--- emulator/cpu/modules/decoder.py
+++ emulator/cpu/modules/decoder.py
@@ -13,25 +13,11 @@ class Decoder():
         # zeropage: ADC oper -- "cont_zp" is the value inside the addres "oper".
         self.cont_zp = self.mem_bus.read(low)
 
-        reg_x = self.cpu.x
-        if reg_x >= 0x80:
-            new_addr_x = low - (~(reg_x - 1) % 256)
-            if new_addr_x >= 0:
-                self.addr_x = new_addr_x
-            else:
-                self.addr_x = low + reg_x
-        else:
-            self.addr_x = low + reg_x
+        local_addr_x = low + self.cpu.x
+        self.addr_x = local_addr_x % 256
 
-        reg_y = self.cpu.y
-        if reg_y >= 0x80:
-            new_addr_y = low - (~(reg_y - 1) % 256)
-            if new_addr_y >= 0:
-                self.addr_y = new_addr_y
-            else:
-                self.addr_y = low + reg_y
-        else:
-            self.addr_y = low + reg_y
+        local_addr_y = low + self.cpu.y
+        self.addr_y = local_addr_y % 256
 
         # zeropage,X: ADC oper,X
         # -- "cont_zp_x" is the value inside the addres ("oper" + x).
@@ -46,8 +32,8 @@ class Decoder():
         # "full_addr" is the address obtained by concatenating "high"|"low" to
         # get the complete 16-bit address.
         self.full_addr = (high << 8) + low
-        self.full_addr_x = (high << 8) + self.addr_x
-        self.full_addr_y = (high << 8) + self.addr_y
+        self.full_addr_x = (high << 8) + local_addr_x
+        self.full_addr_y = (high << 8) + local_addr_y
 
         # absolute: ADC oper -- "content" has the value inside the full address.
         self.content = self.mem_bus.read(self.full_addr)
@@ -72,21 +58,18 @@ class Decoder():
         # Indexed indirect
         # (indirect,X): ADC (oper,X)
         # -- "pointer_content_x" is the value used by the instruction.
-        point_low_x, point_high_x = self.mem_bus.read(self.addr_x, 2)
-        pointer_addr_x = (point_high_x << 8) + point_low_x
-        self.pointer_content_x = self.mem_bus.read(pointer_addr_x)
+        point_low_x = self.mem_bus.read(self.addr_x)
+        point_high_x = self.mem_bus.read((self.addr_x + 1)%256)
+        self.pointer_addr_x = (point_high_x << 8) + point_low_x
+        self.pointer_content_x = self.mem_bus.read(self.pointer_addr_x)
 
         # Indirect indexed
         # (indirect),Y:  ADC (oper),Y
         # -- "pointer_content_y" is the value used by the instruction.
-        point_low_y, point_high_y = self.mem_bus.read(low, 2)
+        point_low_y = self.mem_bus.read(low)
+        point_high_y = self.mem_bus.read((low+1)%256)
 
-        if reg_y >= 0x80:
-            np_low_y = point_low_y - (~(reg_y - 1) % 256)
-        else:
-            np_low_y = point_low_y + reg_y
-
-        pointer_addr_y = (point_high_y << 8) + np_low_y
-        self.pointer_content_y = self.mem_bus.read(pointer_addr_y)
+        self.pointer_addr_y = (point_high_y << 8) + point_low_y + reg_y
+        self.pointer_content_y = self.mem_bus.read(self.pointer_addr_y)
 
         # #####################################################################
