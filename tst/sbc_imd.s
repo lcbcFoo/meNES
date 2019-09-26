baa28b619552f0af33ef165d501336963c132874
Daniel Helú <danielhelu@gmail.com>
2019-09-25 23:11:58 -0300

    Add immediate sbc function and its tests

---
+++ tst/sbc_imd.s
@@ -0,0 +1,99 @@
+; Author: tokumaru
+; http://forums.nesdev.com/viewtopic.php?%20p=58138#58138
+;----------------------------------------------------------------
+; constants
+;----------------------------------------------------------------
+PRG_COUNT = 1 ;1 = 16KB, 2 = 32KB
+MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
+
+;----------------------------------------------------------------
+; variables
+;----------------------------------------------------------------
+
+   .enum $0000
+
+   ;NOTE: declare variables using the DSB and DSW directives, like this:
+
+   ;MyVariable0 .dsb 1
+   ;MyVariable1 .dsb 3
+
+   .ende
+
+   ;NOTE: you can also split the variable declarations into individual pages, like this:
+
+   ;.enum $0100
+   ;.ende
+
+   ;.enum $0200
+   ;.ende
+
+;----------------------------------------------------------------
+; iNES header
+;----------------------------------------------------------------
+
+   .db "NES", $1a ;identification of the iNES header
+   .db PRG_COUNT ;number of 16KB PRG-ROM pages
+   .db $01 ;number of 8KB CHR-ROM pages
+   .db $00|MIRRORING ;mapper 0 and mirroring
+   .dsb 9, $00 ;clear the remaining bytes
+
+;----------------------------------------------------------------
+; program bank(s)
+;----------------------------------------------------------------
+
+   .base $10000-(PRG_COUNT*$4000)
+
+Reset:
+  ;Test only on binary mode
+  cld
+
+  ;Test if A value after the sub is 1 and if carry flag is 1
+  sec
+  lda #2
+  sbc #1
+
+
+  ;Test if carry flag is reset after sub
+  sec
+  sbc #2
+
+  ;Test if Overflow flag is set after sub
+  sec
+  lda #128
+  sbc #1
+
+  clv
+  sec
+  lda #127
+  sbc #-1
+
+
+  ;Test if negative flag is set after sub
+  sec
+  lda #0
+  sbc #1
+
+  ;Test if Zero flag is set after a sum
+  sec
+  lda #1
+  sbc #1
+
+  brk ; Abort execution
+
+NMI:
+
+   ;NOTE: NMI code goes here
+
+IRQ:
+
+   ;NOTE: IRQ code goes here
+
+;----------------------------------------------------------------
+; interrupt vectors
+;----------------------------------------------------------------
+
+   .org $fffa
+
+   .dw NMI
+   .dw Reset
+   .dw IRQ
