048e657ff677bca996d0fc453b801f9a9f079c16
Daniel Helú <danielhelu@gmail.com>
2019-10-16 17:13:17 -0300

    Implement ppu_status.py init and read functions

--- emulator/ppu/registers/ppu_status.py
+++ emulator/ppu/registers/ppu_status.py
@@ -1,5 +1,14 @@
 class PPUSTATUS:
+    VBLANK_STATUS_BIT = 7
+    SPRITE0_HIT_BIT = 6
+    SPRITE_OVERFLOW_BIT = 5
+
+    # I do not know how to decide the initial value to the register.
+    # Ask the teacher about it
     def __init__(self):
-        pass
+        self.reg = 0
+
     def read(self):
-        pass
+        value = self.reg
+        self.reg = ~(1 << VBLANK_STATUS_BIT) & self.reg
+        return self.reg
