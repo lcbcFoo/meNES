bc775140f830b5b5b80ed1a3018995a4543f3851
Daniel Helú <danielhelu@gmail.com>
2019-10-18 17:30:42 -0300

    Add ppu_control register fbit functions

--- emulator/ppu/registers/ppu_control.py
+++ emulator/ppu/registers/ppu_control.py
@@ -1,11 +1,13 @@
 class PPUCTRL:
+
     NMI_BIT = 7
     SLAVE_BIT = 6
     SPRITE_HEIGHT_BIT = 5
     BACKGROUND_PATTERN_TABLE_BIT = 4
     SPRITE_PATTERN_TABLE_BIT = 3
     VRAM_ADDRESS_INCREMENT_BIT = 2
-    BASE_TABLE_ADDRESS = 0
+    BASE_TABLE_ADDRESSH = 1
+    BASE_TABLE_ADDRESSL = 0
 
     def __init__(self, ppu):
         self.ppu = ppu
@@ -14,9 +16,29 @@ class PPUCTRL:
     def reset(self):
         self.reg = 0
 
+    # Write-only
     def read(self):
-        pass
+        return self.reg
 
     def write(self,value):
         self.reg = value
-        pass
+
+    def isNMIEnabled():
+        return (1 << NMI_BIT) & self.reg
+
+    def isSpriteH16():
+        return (1 << SPRITE_HEIGHT_BIT) & self.reg
+
+    def isBackgroundPatternTable1000():
+        return (1 << BACKGROUND_PATTERN_TABLE_BIT) & self.reg
+
+    def isSpritePatternTable1000():
+        return (1 << SPRITE_PATTERN_TABLE_BIT) & self.reg
+
+    def isVRAMAdressIncrement32():
+        return (1 << VRAM_ADDRESS_INCREMENT_BIT) & self.reg
+
+    def returnNameTableAddress():
+        highbit = (1 << BASE_TABLE_ADDRESSH) & self.reg
+        lowbit = (1 << BASE_TABLE_ADDRESSL) & self.reg
+        return highbit + lowbit
