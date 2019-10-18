5dd02c0cc2a441c5c5ab5f2fb24d80ce76e4a2b8
Daniel Helú <danielhelu@gmail.com>
2019-10-18 19:03:51 -0300

    Add ppu_status.py bit functions

--- emulator/ppu/registers/ppu_status.py
+++ emulator/ppu/registers/ppu_status.py
@@ -18,4 +18,15 @@ class PPUSTATUS:
         return self.reg
 
     def write(self, value):
-        pass
+        self.reg = self.reg & 0b11100000
+        tempvalue = value & 0b00011111
+        self.reg = self.reg + tempvalue
+
+    def hasVblankStarted():
+        return (1 << VBLANK_STATUS_BIT) & self.reg
+
+    def isSprite0Hit():
+        return (1 << SPRITE0_HIT_BIT) & self.reg
+
+    def isSpriteOverflowSet():
+        return (1 << SPRITE_OVERFLOW_BIT) & self.reg
