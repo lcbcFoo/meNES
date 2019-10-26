05dc95ce891cd8b89b96a132936a0e8192cb4c63
Laura <politi.laura.o@gmail.com>
2019-10-25 22:19:35 -0300

    Call sprite decoder

--- emulator/ppu/ppu.py
+++ emulator/ppu/ppu.py
@@ -12,57 +12,7 @@ from ppu.registers.ppu_mask import PPUMASK
 from ppu.registers.ppu_scroll import PPUSCROLL
 from ppu.registers.ppu_status import PPUSTATUS
 from ppu.register import Register8Bit, Register16Bit
-# from ppu.sprite_decoder import *
-
-
-
-def split(l, n):
-    for i in range(0, len(l), n):
-        yield l[i:i+n]
-
-def int_array_to_bit_matrix(arr):
-    bit_matrix = []
-    for element in arr:
-        bit_str_array = bin(element)[2:].zfill(8)
-        bit_array = [1 if digit=='1' else 0 for digit in bit_str_array]
-        bit_matrix.append(bit_array)
-    return bit_matrix
-
-def transform_sprites(pattern_tables):
-    my_sprites = list(split(pattern_tables, 8))
-    my_list = []
-    for i in my_sprites:
-        l = int_array_to_bit_matrix(i)
-        my_list.append(l)
-    low, high = my_list[::2], my_list[1::2]
-    ans = generate_sprite_tiles(low, high)
-    # pprint(ans)
-    return ans
-
-
-def generate_sprite_tiles(lo_byte, hi_byte):
-    sprite_tiles = []
-    # For each tile
-    for i in range(len(lo_byte)):
-        new_tyle = []
-        # For each tile line
-        for j in range(len(lo_byte[i])):
-            tile_line = []
-            # For each bit in the line
-            for k in range(len(lo_byte[i][j])):
-                # Get color index and adds it to line
-                pallete_color = lo_byte[i][j][k] + 2 * hi_byte[i][j][k]
-                tile_line.append(pallete_color)
-            # Appends line to tile
-            new_tyle.append(tile_line)
-        # Appends tile to sprites array
-        sprite_tiles.append(new_tyle)
-
-    return sprite_tiles
-
-
-
-
+from ppu.sprite_decoder import *
 
 
 class PPU:
