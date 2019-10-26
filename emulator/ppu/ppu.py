b77fcf4f7a99ec093b405f3af40fd5b2067dcaac
Laura <politi.laura.o@gmail.com>
2019-10-25 22:15:16 -0300

    Call sprite decoder

--- emulator/ppu/ppu.py
+++ emulator/ppu/ppu.py
@@ -1,4 +1,6 @@
 import sys
+import copy
+from pprint import pprint
 
 from ppu.registers.oam_address import OAMADDR
 from ppu.registers.oam_data import OAMDATA
@@ -11,6 +13,57 @@ from ppu.registers.ppu_scroll import PPUSCROLL
 from ppu.registers.ppu_status import PPUSTATUS
 from ppu.register import Register8Bit, Register16Bit
 # from ppu.sprite_decoder import *
+
+
+
+def split(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+def int_array_to_bit_matrix(arr):
+    bit_matrix = []
+    for element in arr:
+        bit_str_array = bin(element)[2:].zfill(8)
+        bit_array = [1 if digit=='1' else 0 for digit in bit_str_array]
+        bit_matrix.append(bit_array)
+    return bit_matrix
+
+def transform_sprites(pattern_tables):
+    my_sprites = list(split(pattern_tables, 8))
+    my_list = []
+    for i in my_sprites:
+        l = int_array_to_bit_matrix(i)
+        my_list.append(l)
+    low, high = my_list[::2], my_list[1::2]
+    ans = generate_sprite_tiles(low, high)
+    # pprint(ans)
+    return ans
+
+
+def generate_sprite_tiles(lo_byte, hi_byte):
+    sprite_tiles = []
+    # For each tile
+    for i in range(len(lo_byte)):
+        new_tyle = []
+        # For each tile line
+        for j in range(len(lo_byte[i])):
+            tile_line = []
+            # For each bit in the line
+            for k in range(len(lo_byte[i][j])):
+                # Get color index and adds it to line
+                pallete_color = lo_byte[i][j][k] + 2 * hi_byte[i][j][k]
+                tile_line.append(pallete_color)
+            # Appends line to tile
+            new_tyle.append(tile_line)
+        # Appends tile to sprites array
+        sprite_tiles.append(new_tyle)
+
+    return sprite_tiles
+
+
+
+
+
 
 class PPU:
 
@@ -56,8 +109,12 @@ class PPU:
 
         # Building sprites matrix
         # self.sprites = decode_sprites(self.mem_bus.pattern_tables)
+        # print(self.mem_bus.pattern_tables)
+
 
         self.reset()
+        self.sprite_table = transform_sprites(self.mem_bus.pattern_tables)
+        pprint(self.sprite_table[0x14])
 
     def set_memory(self, mem_bus):
         self.mem_bus = mem_bus
